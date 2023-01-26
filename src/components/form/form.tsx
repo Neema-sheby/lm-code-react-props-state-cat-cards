a9b108b3180bc66af6a1eba1d844e6bc1677daed
neema <neemadas@gmail.com>
2023-01-26 22:40:38 +0000

    fix: Add useReducer in form.tsx
    Replaced useState with useReducer hook in form.tsx
    
    Added-by: Neema Sheby
    Added-on: 26-06-2023

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import InputField from "./inputField";
 import SelectField from "./selectField";
 import Button from "../button/button";
@@ -11,19 +11,56 @@ interface formProp {
   setAnimalName(T: string): void;
 }
 
+interface State {
+  animal: string;
+  name: string;
+  species: string;
+  favFoods: Array<string>;
+  birthYear: number;
+}
+
+const initialState: State = {
+  animal: "",
+  name: "",
+  species: "",
+  favFoods: [],
+  birthYear: 0,
+};
+
+type Action =
+  | { type: "animal"; payload: string }
+  | { type: "name"; payload: string }
+  | { type: "species"; payload: string }
+  | { type: "favFoods"; payload: Array<string> }
+  | { type: "birthYear"; payload: number };
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "animal":
+      return { ...state, animal: action.payload };
+    case "name":
+      return { ...state, name: action.payload };
+    case "species":
+      return { ...state, species: action.payload };
+    case "favFoods":
+      return { ...state, favFoods: action.payload };
+    case "birthYear":
+      return { ...state, birthYear: action.payload };
+    default:
+      return state;
+  }
+};
+
 const Form: React.FC<formProp> = ({ setData, setAnimalName }) => {
-  const [animal, setAnimal] = useState<string>("");
-  const [name, setName] = useState<string>("");
-  const [species, setSpecies] = useState<string>("");
-  const [favFood, setFavFood] = useState<Array<string>>([]);
-  const [birthYear, setBirthYear] = useState<number>(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { animal, name, species, favFoods, birthYear } = state;
 
   const submitHandler = (e: React.SyntheticEvent) => {
     e.preventDefault();
     const data: AnimalDetail = {
       name: name,
       species: species,
-      favFoods: favFood,
+      favFoods: favFoods,
       birthYear: birthYear,
     };
     setAnimalName(animal);
@@ -35,42 +72,46 @@ const Form: React.FC<formProp> = ({ setData, setAnimalName }) => {
       setData([...dogData, data]);
       console.log(data);
     }
-    setAnimal("");
-    setName("");
-    setSpecies("");
-    setFavFood([]);
-    setBirthYear(0);
+    dispatch({ type: "animal", payload: "" });
+    dispatch({ type: "name", payload: "" });
+    dispatch({ type: "species", payload: "" });
+    dispatch({ type: "favFoods", payload: [] });
+    dispatch({ type: "birthYear", payload: 0 });
   };
 
   return (
     <form className="form" onSubmit={submitHandler}>
       <SelectField
         value={animal}
-        onChange={(e) => setAnimal(e.target.value.toLowerCase())}
+        onChange={(e) =>
+          dispatch({ type: "animal", payload: e.target.value.toLowerCase() })
+        }
       />
 
       <InputField
         name="name"
         value={name}
-        onChange={(e) => setName(e.target.value)}
+        onChange={(e) => dispatch({ type: "name", payload: e.target.value })}
       />
       <InputField
         name="species"
         value={species}
-        onChange={(e) => setSpecies(e.target.value)}
+        onChange={(e) => dispatch({ type: "species", payload: e.target.value })}
       />
       <InputField
         name="favourite foods"
-        value={favFood.join(",")}
+        value={favFoods.join(",")}
         onChange={(e) => {
           const favFoodArray = e.target.value.split(",");
-          setFavFood(favFoodArray);
+          dispatch({ type: "favFoods", payload: favFoodArray });
         }}
       />
       <InputField
         name="birthYear"
         value={birthYear === 0 ? "" : birthYear.toString()}
-        onChange={(e) => setBirthYear(+e.target.value)}
+        onChange={(e) =>
+          dispatch({ type: "birthYear", payload: +e.target.value })
+        }
       />
       <Button className="btn__submit">Add</Button>
     </form>
